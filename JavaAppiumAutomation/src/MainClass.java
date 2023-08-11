a13ad8bab00ea9519c3110eb83b1f06775873daf
Petkevich Anastasiya <stacia31415@gmail.com>
2023-08-11 11:26:20 +0300

    homework1_3

--- JavaAppiumAutomation/src/MainClass.java
+++ JavaAppiumAutomation/src/MainClass.java
@@ -1,9 +1,14 @@
 public class MainClass {
     private static int class_number=20;
+    private static String class_string="Hello, world";
     public static int getClassNumber (){
         return class_number;
     }
     public static int getLocalNumber(){
         return 14;
+    }
+
+    public static String getClassString(){
+        return class_string;
     }
 }
--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -16,4 +16,10 @@ public class MainClassTest {
         int comparativeNumber = 45;
         Assert.assertTrue("actual value is less or equals 45", actual>comparativeNumber );
     }
+
+    @Test
+    public void testGetClassString(){
+        String actual=MainClass.getClassString();
+        Assert.assertTrue("actual string doesn't contain substring \"hello\" or \"Hello\"", actual.contains("Hello")||actual.contains("hello") );
+    }
 }
