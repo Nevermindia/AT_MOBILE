da842b1bbdd54f07a3909347266ead422883fbc3
Petkevich Anastasiya <stacia31415@gmail.com>
2023-08-11 11:13:02 +0300

    homework1_2

--- JavaAppiumAutomation/src/MainClass.java
+++ JavaAppiumAutomation/src/MainClass.java
@@ -1,4 +1,8 @@
 public class MainClass {
+    private static int class_number=20;
+    public static int getClassNumber (){
+        return class_number;
+    }
     public static int getLocalNumber(){
         return 14;
     }
--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -9,4 +9,11 @@ public class MainClassTest {
         Assert.assertEquals("actual value !=14", expected, actual);
 
     }
+
+    @Test
+    public void testGetClassNumber(){
+        int actual = MainClass.getClassNumber();
+        int comparativeNumber = 45;
+        Assert.assertTrue("actual value is less or equals 45", actual>comparativeNumber );
+    }
 }
