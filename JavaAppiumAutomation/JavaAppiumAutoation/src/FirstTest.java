5c4aab0663ee4e71b3238b1ff7363f90fac1c8fd
Petkevich Anastasiya <stacia31415@gmail.com>
2023-09-01 15:11:02 +0300

    Ex5: Тест: сохранение двух статей

--- JavaAppiumAutomation/JavaAppiumAutoation/src/FirstTest.java
+++ JavaAppiumAutomation/JavaAppiumAutoation/src/FirstTest.java
@@ -1,4 +1,5 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import org.junit.After;
@@ -104,6 +105,117 @@ public class FirstTest {
         }
 
     }
+    @Test
+    public void testSaveTwoArticlesAndDeleteOne(){
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find Search Wikipedia input",
+                5
+        );
+        String article_search = "Java";
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                article_search,
+                "Cannot find Search Wikipedia locator",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_description' and @text='Object-oriented programming language']"),
+                "Cannot find locator for text Object-oriented programming language",
+                5
+
+        );
+        waitForElementPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find article title",
+                5
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot find button to save article",
+                5);
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to list']"),
+                "Cannot find 'add to reading list' button",
+                5);
+        String name_of_article_list = "Learning programming";
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_article_list,
+                "Cannot find input to set name for articles folder",
+                5);
+        waitForElementAndClick(
+                By.xpath("//*[@text='OK']"),
+                "Cannot press OK button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@content-desc='Navigate up']"),
+                "Cannot find button to return to the search results list",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_description' and @text='High-level programming language']"),
+                "Cannot find locator for text 'High-level programming language' article",
+                5
+
+        );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot find button to save article",
+                5);
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot find button to save article",
+                5);
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to another reading list']"),
+                "Cannot find 'Add to another reading list' option",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Learning programming']"),
+                "Cannot find " +name_of_article_list +" folder",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='View list']"),
+                "Cannot go to View list",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Learning programming']"),
+                "Cannot find " +name_of_article_list +" folder",
+                5
+        );
+        swipeElementToTheLeft(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find saved article"
+        );
+        waitForElementNotPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Saved article was not deleted",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='High-level programming language']"),
+                "Cannot find second article",
+                5
+
+        );
+        assertElementHasText(
+                By.xpath("//*[@text='JavaScript']"),
+                "JavaScript",
+                "Article doesn't have title 'JavaScript'"
+        );
+
+
+
+
+
+
+
+    }
     private WebElement waitForElementPresent(By by, String error_message, long timeOutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
         wait.withMessage(error_message + "\n");
@@ -142,6 +254,26 @@ public class FirstTest {
         WebElement element = waitForElementPresent(by,"Element not found", 5);
         String actual_text = element.getText().toLowerCase();
         Assert.assertTrue(error_message, actual_text.contains(expected_text.toLowerCase()) );
+
+    }
+    protected void swipeElementToTheLeft(By by, String error_message){
+        WebElement element = waitForElementPresent(
+                 by,
+                error_message,
+                5
+        );
+        int left_x=element.getLocation().getX();
+        int right_x=left_x+element.getSize().getWidth();
+        int lower_y = element.getLocation().getY();
+        int upper_y=lower_y+element.getSize().getHeight();
+        int middle_y=(upper_y+lower_y)/2;
+        TouchAction action = new TouchAction(driver);
+        action
+                .press(right_x,middle_y)
+                .waitAction(150)
+                .moveTo(left_x,middle_y)
+                .release()
+                .perform();
 
     }
 }
